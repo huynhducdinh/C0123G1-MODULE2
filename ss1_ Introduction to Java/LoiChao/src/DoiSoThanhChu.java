99d553cb39b99fb84d2b8f02d3cf6750598c6ea6
huynhducdinh <heeyeon0982002@gmail.com>
2023-03-06 15:49:25 +0700

    main

---
+++ ss1_ Introduction to Java/LoiChao/src/DoiSoThanhChu.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class DoiSoThanhChu {
+    public static void main(String[] args) {
+        System.out.println("Nhập 1 hay nhiều số vào đây");
+        Scanner sc =new Scanner(System.in);
+        int num =Integer.parseInt(sc.nextLine());
+    }
+}
