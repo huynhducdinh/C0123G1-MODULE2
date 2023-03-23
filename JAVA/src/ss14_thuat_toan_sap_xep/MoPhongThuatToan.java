83725d0dd279c5617045f1468a8f2372c7c627f8
huynhducdinh <heeyeon0982002@gmail.com>
2023-03-23 10:12:12 +0700

    main

--- JAVA/src/ss14_thuat_toan_sap_xep/MoPhongThuatToan.java
+++ JAVA/src/ss14_thuat_toan_sap_xep/MoPhongThuatToan.java
@@ -4,25 +4,26 @@ import java.util.Arrays;
 
 public class MoPhongThuatToan {
     public static void main(String[] args) {
-        int arr[]={8,4,9,3,2};
-        System.out.println("Chuỗi chưa được sắp xếp thứ tự: "+Arrays.toString(arr));
+        int arr[] = {8, 4, 9, 3, 2};
+        System.out.println("Chuỗi chưa được sắp xếp thứ tự: " + Arrays.toString(arr));
         insertionSort(arr);
-        System.out.println("Chuỗi sau khi chuyển sắp xếp thứ tự: "+Arrays.toString(arr));
+        System.out.println("Chuỗi sau khi chuyển sắp xếp thứ tự: " + Arrays.toString(arr));
     }
-    public static void insertionSort(int[] array){
+
+    public static void insertionSort(int[] array) {
         int x;
         int pos;
-        for (int i = 0; i < array.length ; i++) {
-            x=array[i];
-            pos=i;
-            while (0<pos&& x<array[pos-1]){
-             array[pos]=array[pos-1];
-             pos--;
-                System.out.println("-lần thứ : "+i+" "+Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            x = array[i];
+            pos = i;
+            while (0 < pos && x < array[pos - 1]) {
+                array[pos] = array[pos - 1];
+                pos--;
+                System.out.println("-lần thứ : " + i + " " + Arrays.toString(array));
 
             }
-            array[pos]=x;
-            System.out.println("+sau lần thứ : "+i+" "+Arrays.toString(array));
+            array[pos] = x;
+            System.out.println("+sau lần thứ : " + i + " " + Arrays.toString(array));
 
         }
     }
