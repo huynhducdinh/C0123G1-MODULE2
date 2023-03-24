e64093d23f3408ac558cd1d71e0082af705213be
huynhducdinh <heeyeon0982002@gmail.com>
2023-03-24 21:42:40 +0700

    main

--- JAVA/src/ss16_io_text/doc_file/DocFile.java
+++ JAVA/src/ss16_io_text/doc_file/DocFile.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class DocFile {
     public static void main(String[] args) {
-        String receive = "src\\ss16_io_text\\doc_file\\Nation.txt";
+        String receive = "src\\ss16_io_text\\doc_file\\Nation.csv";
         List<NationClass> list = readFiles(receive);
         for (NationClass x : list) {
             System.out.println(x.getInfoToCsv());
